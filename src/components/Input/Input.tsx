03c7f3fdc4305c01b314eab69484a3b142469576
vasily <vasilymishanin@gmail.com>
2023-03-12 03:46:36 +0600

    feat: add text input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,21 +1,21 @@
 import './Input.scss';
 import { useState } from 'react';
+import AttachIcon from '../../assets/icon-attach.svg';
+import SendIcon from '../../assets/icon-send.svg';
 
 interface Props {
   placeholder: string;
 }
 
 const CustomTextField: React.FC<Props> = ({ placeholder }) => {
-  const [value, setValue] = useState<string>(''); // state to store the input value
-  const [lines, setLines] = useState<number>(1); // state to track number of lines
+  const [value, setValue] = useState<string>('');
+  const [lines, setLines] = useState<number>(1);
 
-  // function to handle input changes
   const handleChange = (event: React.FormEvent<HTMLDivElement>) => {
     setValue(event.currentTarget.textContent || '');
     setLines(event.currentTarget.childNodes.length);
   };
 
-  // function to prevent default behavior on 'Enter' key press
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
     if (event.key === 'Enter') {
       event.preventDefault();
@@ -23,22 +23,28 @@ const CustomTextField: React.FC<Props> = ({ placeholder }) => {
   };
 
   return (
-    <div className='custom-text-field'>
-      <div
-        className={`custom-text-field__placeholder ${
-          value ? 'custom-text-field__placeholder--hidden' : ''
-        }`}
-      >
-        {placeholder}
+    <div className='text-section'>
+      <div className='custom-text-field'>
+        <div
+          className={`custom-text-field__placeholder ${
+            value ? 'custom-text-field__placeholder--hidden' : ''
+          }`}
+        >
+          {placeholder}
+        </div>
+        <div
+          className={`custom-text-field__input ${
+            lines >= 3 ? 'custom-text-field__input--scroll' : ''
+          }`}
+          contentEditable={true}
+          onInput={handleChange}
+          onKeyDown={handleKeyDown}
+        ></div>
       </div>
-      <div
-        className={`custom-text-field__input ${
-          lines >= 3 ? 'custom-text-field__input--scroll' : ''
-        }`}
-        contentEditable={true}
-        onInput={handleChange}
-        onKeyDown={handleKeyDown}
-      ></div>
+      <div className='text-section__icons'>
+        <img src={AttachIcon} alt='attach' />
+        <img src={SendIcon} alt='send' />
+      </div>
     </div>
   );
 };
